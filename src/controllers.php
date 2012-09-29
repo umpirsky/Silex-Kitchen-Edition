da1c2ef9522aa2620208ab0b388d470aa29b3c6f
Sasa Stamenkovic <umpirsky@gmail.com>
2012-09-29 16:40:26 +0200

    Zymfony Validator examples.

--- src/controllers.php
+++ src/controllers.php
@@ -4,6 +4,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 use Symfony\Component\Validator\Constraints as Assert;
+use Zymfony\Component\Validator\Constraint;
 use Symfony\Component\Form\FormError;
 
 $app->match('/', function() use ($app) {
@@ -65,71 +66,26 @@ $app->match('/form', function() use ($app) {
     $choices = array('choice a', 'choice b', 'choice c');
 
     $form = $builder
-        ->add(
-            $builder->create('sub-form', 'form')
-                ->add('subformemail1', 'email', array(
-                    'constraints' => array(new Assert\NotBlank(), new Assert\Email()),
-                    'attr'        => array('placeholder' => 'email constraints'),
-                    'label'       => 'A custome label : ',
-                ))
-                ->add('subformtext1', 'text')
-        )
-        ->add('text1', 'text', array(
-            'constraints' => new Assert\NotBlank(),
-            'attr'        => array('placeholder' => 'not blank constraints')
+        ->add('post_code', 'text', array(
+            'constraints' => new Constraint(array(
+                'validator' => 'Zend\I18n\Validator\PostCode',
+                'options' => array('locale' => 'en_GB'),
+            ))
         ))
-        ->add('text2', 'text', array('attr' => array('class' => 'span1', 'placeholder' => '.span1')))
-        ->add('text3', 'text', array('attr' => array('class' => 'span2', 'placeholder' => '.span2')))
-        ->add('text4', 'text', array('attr' => array('class' => 'span3', 'placeholder' => '.span3')))
-        ->add('text5', 'text', array('attr' => array('class' => 'span4', 'placeholder' => '.span4')))
-        ->add('text6', 'text', array('attr' => array('class' => 'span5', 'placeholder' => '.span5')))
-        ->add('text8', 'text', array('disabled' => true, 'attr' => array('placeholder' => 'disabled field')))
-        ->add('textarea', 'textarea')
-        ->add('email', 'email')
-        ->add('integer', 'integer')
-        ->add('money', 'money')
-        ->add('number', 'number')
-        ->add('password', 'password')
-        ->add('percent', 'percent')
-        ->add('search', 'search')
-        ->add('url', 'url')
-        ->add('choice1', 'choice',  array(
-            'choices'  => $choices,
-            'multiple' => true,
-            'expanded' => true
+        ->add('domain_name', 'text', array(
+            'constraints' => new Constraint(array(
+                'validator' => 'Zend\Validator\Hostname',
+            ))
         ))
-        ->add('choice2', 'choice',  array(
-            'choices'  => $choices,
-            'multiple' => false,
-            'expanded' => true
+        ->add('iban', 'text', array(
+            'constraints' => new Constraint(array(
+                'validator' => 'Zend\Validator\Iban',
+            ))
         ))
-        ->add('choice3', 'choice',  array(
-            'choices'  => $choices,
-            'multiple' => true,
-            'expanded' => false
-        ))
-        ->add('choice4', 'choice',  array(
-            'choices'  => $choices,
-            'multiple' => false,
-            'expanded' => false
-        ))
-        ->add('country', 'country')
-        ->add('language', 'language')
-        ->add('locale', 'locale')
-        ->add('timezone', 'timezone')
-        ->add('date', 'date')
-        ->add('datetime', 'datetime')
-        ->add('time', 'time')
-        ->add('birthday', 'birthday')
-        ->add('checkbox', 'checkbox')
-        ->add('file', 'file')
-        ->add('radio', 'radio')
-        ->add('password_repeated', 'repeated', array(
-            'type'            => 'password',
-            'invalid_message' => 'The password fields must match.',
-            'options'         => array('required' => true),
-            'first_options'   => array('label' => 'Password'),
-            'second_options'  => array('label' => 'Repeat Password'),
+        ->add('credit_card_number', 'text', array(
+            'constraints' => new Constraint(array(
+                'validator' => 'Zend\Validator\CreditCard',
+            ))
         ))
         ->getForm()
     ;
