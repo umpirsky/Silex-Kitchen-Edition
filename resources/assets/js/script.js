07bf06f17b5dbe187ae00d6fadf8607ee207ff2b
umpirsky <umpirsky@gmail.com>
2012-10-05 16:04:22 +0200

    Experimenting...

--- resources/assets/js/script.js
+++ resources/assets/js/script.js
@@ -1,4 +1,10 @@
 (function($) {
     $(function(){
+        $('.nav a').click(function(e) {
+            e.preventDefault();
+            $.get($(this).attr('href'), function(data) {
+                $('#main').html(data);
+            });
+        });
     });
 })(jQuery);
