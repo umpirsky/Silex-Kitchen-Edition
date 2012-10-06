fa9c1c2a1d64069d7555403d3275f67ebedce82c
Sasa Stamenkovic <umpirsky@gmail.com>
2012-10-06 23:13:36 +0200

    - Handle back and forward.

--- resources/assets/js/script.js
+++ resources/assets/js/script.js
@@ -1,12 +1,38 @@
 (function($) {
     $(function(){
+        // Only if supports pushState
+        if (!(window.history && window.history.pushState)) {
+            return;
+        }
+
+        /**
+         * Loads main content.
+         *
+         * @param String url
+         */
+        var loadContent = function(url) {
+            $.get(url, function(data) {
+                $('#main').html(data);
+            });
+        };
+
+        // Handle back and forward
+        var loaded = false;
+        window.onpopstate = function() {
+            if (!loaded) {
+                loaded = true;
+                return;
+            }
+
+            loadContent(document.location.pathname);
+        };
+
+        // Ajaxify main navigation
         $('.nav a').click(function(e) {
             e.preventDefault();
             var url = $(this).attr('href');
-            $.get(url, function(data) {
-                $('#main').html(data);
-                window.history.pushState(null, null, url);
-            });
+            loadContent(url);
+            window.history.pushState(true, null, url);
         });
     });
 })(jQuery);
